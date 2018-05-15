5437e878d929689d752f26675ec893488971dcea
wangyichen1064431086 <1064431086@qq.com>
2018-05-15 15:23:09 +0800

    Add scss for setting body margin to 0

--- demo/app.js
+++ demo/app.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-//import Nav from '../src/js/index.js';
-import Nav from '../build/index.es.js';
+import Nav from '../src/js/index.js';
+//import Nav from '../build/index.es.js';
 
 import { channelData } from './data.js';
 
